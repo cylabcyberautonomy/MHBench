c9e8b44613306041c35688f6f32ae48041dab535
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-03-13 08:55:33 +0000

    added a start services script to start up sysflow and falco on VMs

--- ansible/defender/__init__.py
+++ ansible/defender/__init__.py
@@ -1,3 +1,4 @@
 from .DeployHoneyService import DeployHoneyService
 from .capabilities.SetupFakeCredential import SetupFakeCredential
 from .sysflow.InstallSysFlow import InstallSysFlow
+from .start_services import StartServices
