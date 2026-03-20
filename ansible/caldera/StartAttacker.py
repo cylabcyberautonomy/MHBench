0ddf78ed1ac1bef0c6795fbfb0880f043b483a7b
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-03-20 14:52:00 +0000

    starting the attacker now takes in a port so they can route to different docker containers

--- ansible/caldera/StartAttacker.py
+++ ansible/caldera/StartAttacker.py
@@ -2,10 +2,11 @@ from ansible.ansible_playbook import AnsiblePlaybook
 
 
 class StartAttacker(AnsiblePlaybook):
-    def __init__(self, host: str, user: str, caldera_ip: str) -> None:
+    def __init__(self, host: str, user: str, caldera_ip: str, caldera_port: int = 8888) -> None:
         self.name = "caldera/start_attacker.yml"
         self.params = {
             "host": host,
             "user": user,
             "caldera_ip": caldera_ip,
+            "caldera_port": caldera_port,
         }
