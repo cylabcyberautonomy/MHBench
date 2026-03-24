03787db5979b7661bea4a2140ac0ab18eef2609c
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-03-24 15:51:27 +0000

    added ICS terraform files

---
+++ src/environments/terraform/topologies/ics_inspired/providers.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "1.54.1"
+    }
+  }
+}
+
+# No default value
+variable "project_name" {
+  type        = string
+  description = "Openstack project"
+}
+
+# No default value
+variable "openstack_username" {
+  type        = string
+  description = "Openstack username"
+}
+
+# default value for the variable location
+variable "openstack_password" {
+  type        = string
+  description = "Openstack password"
+}
+
+variable "openstack_auth_url" {
+  type        = string
+  description = "Openstack auth url"
+}
+
+variable "openstack_region" {
+  type        = string
+  description = "Openstack region"
+}
+
+variable "perry_key_name" {
+  type        = string
+  description = "Name of the keypair to use for the Perry instance"
+}
+
+variable "images" {
+  type = object({
+    ubuntu            = string
+    ubuntu_pip        = string
+    kali              = string
+    webserver_baked   = optional(string, "")
+    database_baked    = optional(string, "")
+    employee_baked    = optional(string, "")
+    attacker_baked    = optional(string, "")
+    manage_host_baked = optional(string, "")
+    host_baked        = optional(string, "")
+  })
+}
+
+variable "flavors" {
+  type = object({
+    tiny   = string
+    small  = string
+    medium = string
+    large  = string
+    huge   = string
+  })
+}
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = var.openstack_username
+  tenant_name = var.project_name
+  password    = var.openstack_password
+  auth_url    = var.openstack_auth_url
+  region      = var.openstack_region
+  insecure    = "true"
+}
+
+
