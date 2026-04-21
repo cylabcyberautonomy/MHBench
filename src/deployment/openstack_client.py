043e05f8a4623792b40805f6b835c86c2630d7a7
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-04-21 19:08:23 +0000

    openstack config now uses cloud.yaml instead of bespoke python

--- src/deployment/openstack_client.py
+++ src/deployment/openstack_client.py
@@ -11,17 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 def build_connection(cfg: OpenStackConfig) -> Connection:
-    logger.debug("Connecting to %s (project: %s)", cfg.auth_url, cfg.project_name)
-    conn = openstack.connect(
-        auth_url=cfg.auth_url,
-        username=cfg.username,
-        password=cfg.password,
-        project_name=cfg.project_name,
-        region_name=cfg.region,
-        identity_api_version=3,
-        user_domain_name="Default",
-        project_domain_name="Default",
-        api_timeout=cfg.client_timeout,
-    )
+    logger.debug("Connecting to cloud '%s'", cfg.cloud)
+    kwargs = {"cloud": cfg.cloud}
+    if cfg.clouds_yaml:
+        kwargs["config_files"] = [cfg.clouds_yaml]
+    conn = openstack.connect(**kwargs)
     logger.debug("Connection established.")
     return conn
